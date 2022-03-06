2841576fec4f9b2117efb68e056fdfd9391d834e
smirkinhd <smirkinhd@gmail.com>
2022-03-06 10:24:12 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 397);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 403);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 47);
+            this.panel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(648, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Вперёд";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать папку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        List<string> imgList = new List<string>();
+        int nImg = 0; // номер (в списке) отображаемой иллюстрации
+        string aPath; // путь к файлам
+
+        public Form1()
+        {
+            InitializeComponent();
+            DirectoryInfo di; // каталог
+                              // получить имя каталога "Мои рисунки"
+            di = new DirectoryInfo(Environment.GetFolderPath(
+            Environment.SpecialFolder.MyPictures));
+            aPath = di.FullName;
+            FillListBox(aPath);
+
+        }
+        private Boolean FillListBox(string aPath)
+        {
+            // информация о каталоге
+            DirectoryInfo di = new DirectoryInfo(aPath);
+            // информация о файлах
+            FileInfo[] fi = di.GetFiles("*.jpg");
+            // очистить список иллюстраций
+            imgList.Clear();
+            // добавляем в imgList имена jpg-файлов каталога aPath
+            foreach (FileInfo fc in fi)
+            {
+                imgList.Add(fc.Name);
+            }
+            if (fi.Length == 0)
+            {
+                button2.Enabled = false;
+                button3.Enabled = false;
+                return false;
+            }
+            else
+            {
+                nImg = 0;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                // сделать недоступной кнопку Назад
+                button2.Enabled = false;
+                // если в каталоге один jpg-файл, сделать недоступной кнопку Вперёд
+                if (imgList.Count == 1)
+                    button3.Enabled = false;
+                else
+                    button3.Enabled = true;
+                return true;
+            }
+
+        }
+        private void ShowPicture(string aPicture)
+        {
+            pictureBox1.Visible = false;
+            // загружаем изображение в pictureBox1
+            pictureBox1.Image = System.Drawing.Bitmap.FromFile(aPicture);
+            // надо масштабировать?
+            if ((pictureBox1.Image.Width > pictureBox1.Width) ||
+            (pictureBox1.Image.Height > pictureBox1.Height))
+            { // да
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else // нет, не надо масштабировать
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.Visible = true;
+            this.Text = aPicture;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // если кнопка "Вперёд" недоступна, сделаем ее доступной
+            if (!button3.Enabled)
+                button3.Enabled = true;
+            if (nImg > 0)
+            {
+                nImg--;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                // отображается первая иллюстрация
+                if (nImg == 0)
+                { // теперь кнопка Назад недоступна
+                    button2.Enabled = false;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!button2.Enabled)
+                button2.Enabled = true;
+            if (nImg < imgList.Count)
+            {
+                nImg++;
+                ShowPicture(aPath + "\\" + imgList[nImg]);
+                if (nImg == imgList.Count - 1)
+                {
+                    button3.Enabled = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // FolderBrowserDialog - стандартное окно Обзор папок
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.Description = "Выберите папку в которой\nнаходятся иллюстрации";
+            // кнопка Создать папку недоступна
+            fb.ShowNewFolderButton = false;
+            // "стартовая" папка
+            fb.SelectedPath = aPath;
+            // отображаем диалоговое окно
+            if (fb.ShowDialog() == DialogResult.OK)
+            { // пользователь выбрал каталог и щелкнул на кнопке ОК
+                aPath = fb.SelectedPath;
+                if (!FillListBox(fb.SelectedPath))
+                    // в каталоге нет файлов иллюстраций
+                    pictureBox1.Image = null;
+            }
+
+        }
+    }
+}
